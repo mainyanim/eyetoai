229b758e48b8f1a646741d6d76c44480468eaa19
mainyanim <alicejarmusch@gmail.com>
2017-07-19 18:42:39 +0300

    can count C_i

--- database/predict.py
+++ database/predict.py
@@ -1,10 +1,10 @@
 import pyodbc
 
-config = dict(server=   'tcp:eyetoaidevelopment.database.windows.net', # change this to your SQL Server hostname or IP address
-              port=      1433,                    # change this to your SQL Server port number [1433 is the default]
-              database= 'test',
-              username= 'michael',
-              password= 'GradioAI7000')
+config = dict(server = 'tcp:eyetoaidevelopment.database.windows.net',
+              port = 1433,
+              database = 'test',
+              username = 'michael',
+              password = 'GradioAI7000')
 
 
 conn_str = ('SERVER={server},{port};'   +
@@ -20,7 +20,21 @@ conn = pyodbc.connect(
 
 cursor = conn.cursor()
 
-cursor.execute('SELECT * FROM reports')
+def get_c_i(c):
+    cursor.execute("SELECT Max(ReportId) FROM ReportConditionFindingOptions")
+    total = cursor.fetchval()
+    cursor.execute("SELECT COUNT (Conditions.Name) from Conditions INNER JOIN ReportConditionFindingOptions ON Conditions.Id = ReportConditionFindingOptions.ConditionId WHERE Name = ?", c)
+    freq = cursor.fetchval()
+    print("{0} / {1} = {2}".format(freq, total, freq / total))
 
-for entry in cursor:
-    print(entry)
+def get_f_j(f):
+    cursor.execute("SELECT COUNT(*) FROM Findings")
+    total = cursor.fetchval()
+    cursor.execute("SELECT COUNT(*) FROM Findings WHERE Name=?", f)
+    freq = cursor.fetchval()
+    print("{0} / {1} = {2}".format(freq, total, freq / total))
+
+if __name__ == '__main__':
+    get_c_i('Fibroadenoma')
+    get_f_j('Mass')
+
