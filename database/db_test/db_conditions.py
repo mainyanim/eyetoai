a0f2bdf9cb4bce58dd75337b5143fb8fc9911699
mainyanim <alicejarmusch@gmail.com>
2017-08-18 14:26:26 +0300

    finished with mri

--- database/db_test/db_conditions.py
+++ database/db_test/db_conditions.py
@@ -68,13 +68,13 @@ class Condition:
         self.us_lymph_n_loc = [13]
         self.us_sp_c = [14]
         # MRI
-        self.mri_mass = [15, 16, 17]
-        self.mri_features = [18]
-        self.mri_kca_ = [19]
-        self.mri_nme = [20, 21]
-        self.mri_nef = [22]
-        self.mri_lymph_n = [23]
-        self.mri_fcl = [24]
+        self.mri_mass_loc = [15, 16, 17]
+        self.mri_features_loc = [18]
+        self.mri_kca_loc = [19]
+        self.mri_nme_loc = [20, 21]
+        self.mri_nef_loc = [22]
+        self.mri_lymph_n_loc = [23]
+        self.mri_fcl_loc = [24]
 
     @classmethod
     def get_random_parameter(self, row):
@@ -154,7 +154,7 @@ def create_report(infile):
                           mammo_findings=mammo_findings_list,us_findings=us_findings_list,
                           mri_findings=mri_findings_list)
     #modality = random.choice(condition.modalities)
-    modality = 'US'
+    modality = 'MRI'
     report['modality'] = modality
 
 
@@ -274,6 +274,95 @@ def create_report(infile):
             return ret
     else:
         findings_list = condition.mri_findings
+        arr_temp = []
+        for i in range(random.randrange(1, len(findings_list))):
+            rand_item = random.choice(mri_findings_list)
+            arr_temp.append(rand_item)
+
+        # list of dictionaries for findings
+        report['conditions'][condname] = {'findings': [{'name': x} for x in arr_temp]}
+
+        # create a dict for mass parameters
+        mass_ps = create_loc_dict(mri_mass_params, condition.mri_mass_loc)
+        mass_lst = [*mass_ps]
+        m_params_lst = [{'name': _} for _ in mass_lst]
+
+        # report['conditions'][condname]['findings'] = {'parameters': [{'name': _} for _ in mass_lst]} overrides existed structure
+        features_ps = create_loc_dict(mri_mri_features_params, condition.mri_features_loc)
+        features_lst = [*features_ps]
+        features_params_lst = [{'name': _} for _ in features_lst]
+
+        kin_ca_ps = create_loc_dict(mri_kin_c_a_params , condition.mri_kca_loc)
+        kin_ca_lst = [*kin_ca_ps]
+        kin_ca_params_lst = [{'name': _} for _ in kin_ca_lst]
+
+        nme_ps = create_loc_dict(mri_nme_params, condition.mri_nme_loc)
+        nme_ps_lst = [*nme_ps]
+        nme_params_lst = [{'name': _} for _ in nme_ps_lst]
+
+        nef_ps = create_loc_dict(mri_nef_params, condition.mri_nef_loc)
+        nef_ps_lst = [*nef_ps]
+        nef_params_lst = [{'name': _} for _ in nef_ps_lst]
+
+        l_nodes_ps = create_loc_dict(mri_lymph_nodes_params, condition.mri_lymph_n_loc)
+        l_nodes_lst = [*l_nodes_ps]
+        l_nodes_params_lst = [{'name': _} for _ in l_nodes_lst]
+
+        fcl_ps = create_loc_dict(mri_fcl_params, condition.mri_fcl_loc)
+        fcl_ps_lst = [*fcl_ps]
+        fcl_params_lst = [{'name': _} for _ in fcl_ps_lst]
+
+        def construct_dict(x):
+            ret = {'name': x}
+            if x == mri_findings_list[0]:
+                new_par_mass = []
+                # a loop for getting random option for each parameter in a finding (as a test used mass)
+                for k in range(len(mass_lst)):
+                    new_par_mass += [{'value': x} for x in condition.get_random_parameter(mass_ps[mass_lst[k]])]
+                paired_vals_mass = [{**x, **y} for (x, y) in zip(m_params_lst, new_par_mass)]
+                ret['parameters'] = paired_vals_mass
+            elif x == mri_findings_list[1]:
+                new_par_feat = []
+                for k in range(len(features_lst)):
+                    new_par_feat+= [{'value': x} for x in
+                                         condition.get_random_parameter(features_ps[features_lst[k]])]
+                paired_vals_feat = [{**x, **y} for (x, y) in zip(us_sp_cases_params_lst, new_par_feat)]
+                ret['parameters'] = paired_vals_feat
+            elif x == mri_findings_list[2]:
+                new_par_kca = []
+                for k in range(len(kin_ca_lst)):
+                    new_par_kca += [{'value': x} for x in condition.get_random_parameter(kin_ca_ps[kin_ca_lst[k]])]
+                paired_vals_kca = [{**x, **y} for (x, y) in zip(kin_ca_params_lst, new_par_kca)]
+                ret['parameters'] = paired_vals_kca
+            elif x == mri_findings_list[3]:
+                new_par_nme = []
+                for k in range(len(nme_ps_lst)):
+                    new_par_nme += [{'value': x} for x in
+                                         condition.get_random_parameter(nme_ps[nme_ps_lst[k]])]
+                paired_vals_nme = [{**x, **y} for (x, y) in zip(nme_params_lst, new_par_nme)]
+                ret['parameters'] = paired_vals_nme
+            elif x == mri_findings_list[4]:
+                new_par_nef = []
+                for k in range(len(nef_ps_lst)):
+                    new_par_nef += [{'value': x} for x in
+                                         condition.get_random_parameter(nef_ps[nef_ps_lst[k]])]
+                paired_vals_nef = [{**x, **y} for (x, y) in zip(nef_params_lst, new_par_nef)]
+                ret['parameters'] = paired_vals_nef
+            elif x == mri_findings_list[5]:
+                new_par_l_nodes = []
+                for k in range(len(l_nodes_lst)):
+                    new_par_l_nodes += [{'value': x} for x in
+                                        condition.get_random_parameter(l_nodes_ps[l_nodes_lst[k]])]
+                paired_vals_l_nodes = [{**x, **y} for (x, y) in zip(l_nodes_params_lst, new_par_l_nodes)]
+                ret['parameters'] = paired_vals_l_nodes
+            elif x == mri_findings_list[6]:
+                new_par_fcl = []
+                for k in range(len(fcl_ps_lst)):
+                    new_par_fcl += [{'value': x} for x in
+                                    condition.get_random_parameter(fcl_ps[fcl_ps_lst[k]])]
+                paired_vals_fcl = [{**x, **y} for (x, y) in zip(fcl_params_lst, new_par_fcl)]
+                ret['parameters'] = paired_vals_fcl
+            return ret
 
 
 
