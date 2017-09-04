8aa62753d5eec6728dace06beb3ca065552651d5
mainyanim <alicejarmusch@gmail.com>
2017-09-04 16:05:17 +0300

    mongo atlas connected

--- database/predict_upd.py
+++ database/predict_upd.py
@@ -3,27 +3,37 @@ import numpy as np
 from flask_api import FlaskAPI, status, exceptions
 from flask import request, abort
 import json, requests
+import ssl
 
+"""
+author: Alexandra Zhuravlyov
+"""
+
+client = MongoClient("mongodb://yuri:rxLPDCJPXWHOozeV@cluster0-shard-00-00-lnxfa.mongodb.net:27017,cluster0-shard-00-01-lnxfa.mongodb.net:27017,cluster0-shard-00-02-lnxfa.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin")
+db = client.eyetoai
+
+"""
+pymongo driver for local usage
 client = MongoClient('localhost', 27017)
-db = client.reports
-reportsColl = db.reportsColl
+db  = client.reports
+"""
+reports = db.reports
 app = FlaskAPI(__name__)
 
 
 def cond_freq(condition):
-    cursor_cond = db.reportsColl.aggregate([
-        {"$unwind": "$conditions"},
-        {"$group": {"_id": "$_id", "sum": {"$sum": 1}}}])
+    cursor_cond = db.reports.aggregate([{"$unwind": "$Conditions"},
+                                         {"$group": {"_id": "$_id", "sum": {"$sum": 1}}}])
 
     total = sum(result['sum'] for result in cursor_cond)
-    cursor_c0 = db.reportsColl.find({"conditions.conditionName": condition}).count()
+    cursor_c0 = db.reports.find({"Conditions.ConditionName": condition}).count()
     freq = cursor_c0 / total
     return freq
 
 
 def finding_condition(finding, condition):
-    cursor_finding = db.reportsColl.find({"$and": [{"conditions.findings.name": finding},
-                                                   {"conditions.conditionName": condition}]}).count()
+    cursor_finding = db.reports.find({"$and": [{"Conditions.Findings.Name": finding},
+                                                   {"Conditions.ConditionName": condition}]}).count()
     return cursor_finding
 
 
@@ -54,4 +64,7 @@ def predict():
 if __name__ == '__main__':
     app.run(host = 'localhost', port = 8000, debug = True)
 
-#curl -X POST -H "Content-Type: application/json" -d "{ \"condition\": \"Fibroadenoma\",\"findings\": [\"Mass\"] }" http://localhost:8000/
+"""
+test curl request for getting a result:
+curl -X POST -H "Content-Type: application/json" -d "{ \"condition\": \"Fibroadenoma\",\"findings\": [\"Mass\"] }" http://localhost:8000/
+"""
