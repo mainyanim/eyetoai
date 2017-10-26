74b0ae78fff9d23a104f3c5b8eb96c28637d92bc
mainyanim <alicejarmusch@gmail.com>
2017-10-26 17:38:11 +0300

    normalized arr

--- database/interface.py
+++ database/interface.py
@@ -1,6 +1,9 @@
 from pymongo import MongoClient
 from db_config import *
 import re
+from flask_api import FlaskAPI, status, exceptions
+from flask import request, abort
+import json, requests
 
 """
 author: Alexandra Zhuravlyov
@@ -11,53 +14,41 @@ client = MongoClient("mongodb://yuri:rxLPDCJPXWHOozeV@cluster0-shard-00-00-lnxfa
                      "cluster0-shard-00-01-lnxfa.mongodb.net:27017,cluster0-shard-00-02-lnxfa.mongodb.net:27017/test?ssl=true&replicaSet="
                      "Cluster0-shard-0&authSource=admin")
 db = client.eyetoai
+app = FlaskAPI(__name__)
 
 
 def ddata():
-    modality = input('Modality is: ')
-    #cond = input('Condition you want to check is: ').title()
-    find_arr = db.fill.distinct("conditions.findings.name", {"modality": re.compile(modality, re.IGNORECASE)})
-
-    num_findings = int(input('How many findings do you have: '))
+    num_findings = len(request.json.get("findings"))
+    findings = request.json.get("findings")
+    print(findings)
+    print(num_findings)
+    modality = request.json.get("modality")
     user_input = []
-    for _ in range(num_findings):
+    for f in range(num_findings):
         finding_input = {}
-        ch_f = input('Choose finding from the list: ' + str(find_arr))
+        ch_f = request.json.get("findings")[f]["name"]
         finding_input["name"] = ch_f
+        params = request.json.get("findings")[f]["parameters"]
         parameters_input = []
-        if ch_f!='Mass':
-            names = list(db.fill.aggregate(get_parameter_names(modality, ch_f)))[0]['uniqueValues']
-        else:
-            names = list(db.fill.aggregate(get_parameter_names(modality, ch_f)))[0]['uniqueValues']
-            print(names)
-            if 'Location' in names:
-                names.remove('Location')
-        for name in names:
-            values_arr = list(db.fill.aggregate(get_value(modality, ch_f, name)))[0]['uniqueValues']
-            value = str(input(name + ' is: ' + str(values_arr)))
+        for param in range(len(params)):
+            name = params[param]["name"]
+            value = params[param]["value"]
             parameters_input.append((name, value))
-        finding_input["parameters"] = parameters_input
+            finding_input["parameters"] = parameters_input
+            print(finding_input)
         user_input.append(finding_input)
 
-    """
-    cursor_findings = [db.qa2_lnodes.find(get_f_params_val(modality, cond, ui)).count() for ui in user_input]
-    print("number of conditions with selected parameters is: ", cursor_findings)
-    if 0 in cursor_findings:
-        print("Finding with selected parameters doesn't exist in the database")
-    selected_f = min(cursor_findings) # total number of condition with values
-    print("selected f are: ", selected_f)
-    """
+
     probs = []
     db_condlist = db.reportsNew.distinct("conditions.conditionName")
-
-    for condition in db_condlist:
-        cursor_findings = [db.fill.find(get_f_params_val(modality, condition, ui)).count() for ui in user_input]
+    for condition in request.json.get("conditions"):
+        cursor_findings = [db.fill.find(get_f_params_val(modality, condition["name"], ui)).count() for ui in user_input]
         print("number of conditions with selected parameters is: ", cursor_findings)
         if 0 in cursor_findings:
             #print("Finding with selected parameters doesn't exist in the database")
             pass
         selected_f = min(cursor_findings)  # total number of condition with values
-        cursor_c0 = db.fill.find({"conditions.conditionName": condition}).count()  # total number of specific conditions
+        cursor_c0 = db.fill.find({"conditions.conditionName": condition["name"]}).count()  # total number of specific conditions
         if cursor_c0:
             cursor_cond = db.fill.aggregate([{"$unwind": "$conditions"}, {"$group": {"_id": "$_id", "sum": {"$sum": 1}}}])
             total = sum(result['sum'] for result in cursor_cond) # total number of reports in the database (splitted)
@@ -66,7 +57,7 @@ def ddata():
                 p_ci = cursor_c0 / total # specific condition / total num of reports
                 prob = p_v_ci * p_ci # P(Ci | val)
                 if prob!=0:
-                    probs+=[{"id": condition, "prob": prob}]
+                    probs+=[{"id": condition["id"], "ranking": prob}]
     #probs.sort()
 
     print(probs)
@@ -76,5 +67,14 @@ def ddata():
     #print("Prob is: ", prob)
 
 
+@app.route("/", methods=['POST'])
+def predict():
+    if not request.json:
+        abort(400)
+    print(request.json)
+
+    result = str(ddata())
+    return result
+
 if __name__ == '__main__':
-    ddata()
+    app.run(host = 'localhost', port = 8080, debug = True)
