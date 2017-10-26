5e79b013346013928065243a17dfeae3c6d75642
mainyanim <alicejarmusch@gmail.com>
2017-10-26 11:29:03 +0300

    regex

--- database/db_config.py
+++ database/db_config.py
@@ -1,23 +1,25 @@
+import re
+
 def get_f_params_val(modality, cond, user_input):
     return {
         "$and": [
-            {"modality": modality},
+            {"modality": re.compile(modality, re.IGNORECASE)},
             {"conditions": {
                 "$elemMatch": {
-                    "conditionName": cond,
+                    "conditionName": re.compile(cond, re.IGNORECASE) ,
                     "findings": {
                         "$elemMatch": {
-                            "name": user_input["name"],
+                            "name": re.compile(user_input["name"], re.IGNORECASE),
                             "$and": [
                                 {
                                     "parameters": {
                                         "$elemMatch": {
-                                            "name": query_tuple[0],
-                                            "value": query_tuple[1]
+                                            "name": re.compile(query_tuple[0], re.IGNORECASE),
+                                            "value": re.compile(query_tuple[1], re.IGNORECASE)
                                         }
                                     }
                                 }
-                                for query_tuple in user_input["parameters"]]
+                            for query_tuple in user_input["parameters"]]
                         }
                     }
                 }
@@ -29,7 +31,7 @@ def get_f_params_val(modality, cond, user_input):
 def get_value(modality, finding, parameter):
     return [
         {
-            "$match": {"modality": modality}
+            "$match": {"modality": re.compile(modality, re.IGNORECASE)}
         },
         {
             "$unwind": "$conditions"
@@ -38,13 +40,13 @@ def get_value(modality, finding, parameter):
             "$unwind": "$conditions.findings"
         },
         {
-            "$match": {"conditions.findings.name": finding}
+            "$match": {"conditions.findings.name": re.compile(finding, re.IGNORECASE)}
         },
         {
             "$unwind": "$conditions.findings.parameters"
         },
         {
-            "$match": {"conditions.findings.parameters.name": parameter}
+            "$match": {"conditions.findings.parameters.name": re.compile(parameter, re.IGNORECASE)}
         },
         {
             "$group": {"_id": "null", "uniqueValues": {"$addToSet": "$conditions.findings.parameters.value"}}
@@ -55,7 +57,7 @@ def get_value(modality, finding, parameter):
 def get_parameter_names(modality, finding):
     return [
         {
-            "$match": {"modality": modality}
+            "$match": {"modality": re.compile(modality, re.IGNORECASE)}
         },
         {
             "$unwind": "$conditions"
@@ -64,7 +66,7 @@ def get_parameter_names(modality, finding):
             "$unwind": "$conditions.findings"
         },
         {
-            "$match": {"conditions.findings.name": finding}
+            "$match": {"conditions.findings.name": re.compile(finding, re.IGNORECASE)}
         },
         {
             "$unwind": "$conditions.findings.parameters"
--- database/interface.py
+++ database/interface.py
@@ -1,12 +1,6 @@
 from pymongo import MongoClient
-from cond_data import *
 from db_config import *
-import collections
-import numpy as np
-from flask import request, abort
-import math
-import json, requests
-import ssl
+import re
 
 """
 author: Alexandra Zhuravlyov
@@ -22,7 +16,7 @@ db = client.eyetoai
 def ddata():
     modality = input('Modality is: ')
     #cond = input('Condition you want to check is: ').title()
-    find_arr = db.reportsNew.distinct("conditions.findings.name", {"modality": modality})
+    find_arr = db.fill.distinct("conditions.findings.name", {"modality": re.compile(modality, re.IGNORECASE)})
 
     num_findings = int(input('How many findings do you have: '))
     user_input = []
@@ -32,18 +26,19 @@ def ddata():
         finding_input["name"] = ch_f
         parameters_input = []
         if ch_f!='Mass':
-            names = list(db.reportsNew.aggregate(get_parameter_names(modality, ch_f)))[0]['uniqueValues']
+            names = list(db.fill.aggregate(get_parameter_names(modality, ch_f)))[0]['uniqueValues']
         else:
-            names = list(db.reportsNew.aggregate(get_parameter_names(modality, ch_f)))[0]['uniqueValues']
+            names = list(db.fill.aggregate(get_parameter_names(modality, ch_f)))[0]['uniqueValues']
             print(names)
             if 'Location' in names:
                 names.remove('Location')
         for name in names:
-            values_arr = list(db.reportsNew.aggregate(get_value(modality, ch_f, name)))[0]['uniqueValues']
+            values_arr = list(db.fill.aggregate(get_value(modality, ch_f, name)))[0]['uniqueValues']
             value = str(input(name + ' is: ' + str(values_arr)))
             parameters_input.append((name, value))
         finding_input["parameters"] = parameters_input
         user_input.append(finding_input)
+
     """
     cursor_findings = [db.qa2_lnodes.find(get_f_params_val(modality, cond, ui)).count() for ui in user_input]
     print("number of conditions with selected parameters is: ", cursor_findings)
@@ -56,22 +51,22 @@ def ddata():
     db_condlist = db.reportsNew.distinct("conditions.conditionName")
 
     for condition in db_condlist:
-        cursor_findings = [db.reportsNew.find(get_f_params_val(modality, condition, ui)).count() for ui in user_input]
+        cursor_findings = [db.fill.find(get_f_params_val(modality, condition, ui)).count() for ui in user_input]
         print("number of conditions with selected parameters is: ", cursor_findings)
         if 0 in cursor_findings:
             #print("Finding with selected parameters doesn't exist in the database")
             pass
         selected_f = min(cursor_findings)  # total number of condition with values
-        cursor_c0 = db.reportsNew.find({"conditions.conditionName": condition}).count()  # total number of specific conditions
+        cursor_c0 = db.fill.find({"conditions.conditionName": condition}).count()  # total number of specific conditions
         if cursor_c0:
-            cursor_cond = db.reportsNew.aggregate([{"$unwind": "$conditions"}, {"$group": {"_id": "$_id", "sum": {"$sum": 1}}}])
+            cursor_cond = db.fill.aggregate([{"$unwind": "$conditions"}, {"$group": {"_id": "$_id", "sum": {"$sum": 1}}}])
             total = sum(result['sum'] for result in cursor_cond) # total number of reports in the database (splitted)
             if total:
                 p_v_ci = selected_f / cursor_c0 # P(Values | Ci)
                 p_ci = cursor_c0 / total # specific condition / total num of reports
                 prob = p_v_ci * p_ci # P(Ci | val)
                 if prob!=0:
-                    probs+=[{"name": condition, "prob": prob}]
+                    probs+=[{"id": condition, "prob": prob}]
     #probs.sort()
 
     print(probs)
@@ -79,33 +74,6 @@ def ddata():
     probs_sorted = sorted(probs, key=itemgetter('prob'))[::-1]
     print(probs_sorted)
     #print("Prob is: ", prob)
-
-
-
-    """
-            for cursor_finding in cursor_findings:
-                
-                # print(cursor_finding)
-                # print('total is', total)
-                # print('cursor is ', cursor_c0)
-                # print(freq)  # condition frequency = P(Ci)
-                prob = cursor_finding / cursor_c0
-                probs.append(prob)
-                # print(prob)
-            if (sum(probs)!=0):
-                print("Frequency is " + str(freq))
-                print("Probabilities are " + str(probs))
-                probs_norm = [float(i) / sum(probs) for i in probs]
-                result = np.prod(np.array(probs_norm))*freq
-                print("Result is " + str(result))
-            else:
-                result = np.prod(np.ones((len(cursor_findings),), dtype=np.int)) * freq
-                print("Result is " + str(result))
-        else:
-            print("Finding with selected parameters doesn't exist in the database")
-    else:
-        print("Condition doesn't exist")
-    """
 
 
 if __name__ == '__main__':
