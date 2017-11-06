13d7d67a123b425b249ada8e6b01467e57d15941
mainyanim <alicejarmusch@gmail.com>
2017-11-07 00:14:34 +0200

    test

--- database/prob_mtrx_gen.py
+++ database/prob_mtrx_gen.py
@@ -28,17 +28,17 @@ H = - E *log 2 (P)
 import itertools
 
 
-findings = db.fill.distinct("conditions.findings.name", {"modality": "Mammography"})
+findings = db.test_entr.distinct("conditions.findings.name", {"modality": "Mammography"})
 #print(findings)
 #check if query works
 
 def get_params():
     elems = []
     for elem in findings:
-        names = list(db.fill.aggregate(get_parameter_names("Mammography", elem)))[0]['uniqueValues']
+        names = list(db.test_entr.aggregate(get_parameter_names("Mammography", elem)))[0]['uniqueValues']
         elems_tmp = []
         for name in names:
-            values_arr = list(db.fill.aggregate(get_value("Mammography", elem, name)))[0]['uniqueValues']
+            values_arr = list(db.test_entr.aggregate(get_value("Mammography", elem, name)))[0]['uniqueValues']
             elems_tmp += [values_arr]
         elems+=[elems_tmp]
     return elems
@@ -47,7 +47,7 @@ def get_params():
     # check if works for f_list - should reduce next lines
 
 def mean(arr):
-    return float(sum(arr)) / max(len(arr), 1)
+    return float(sum(arr)) / (len(arr))
 
 
 def cartesian(arrays, out=None):
@@ -69,17 +69,33 @@ new_data = []
 for e in get_params():
     new_data+=[cartesian(e)]
 #print(new_data)
-db_condlist = db.fill.distinct("conditions.conditionName")
-print(new_data[0], new_data[1], new_data[3])
+db_condlist = db.test_entr.distinct("conditions.conditionName")
+print(new_data)
 
-mass_cond = []
+def cond_freq(condition):
+    cursor_cond = db.reportsNew.aggregate([{"$unwind": "$conditions"},
+                                         {"$group": {"_id": "$_id", "sum": {"$sum": 1}}}])
+
+    total = sum(result['sum'] for result in cursor_cond)
+    cursor_c0 = db.reportsNew.find({"conditions.conditionName": condition}).count()
+    freq = cursor_c0 / total
+    print(freq)
+    return freq
+
+
+#new_data[0] - lnodes
+#new_data[1] - mass
+#new_data[2] - assymetry
+#new_data[3] - calc
+
 calc_cond = []
+mass_cond = []
 assym_cond = []
-lnodes_cond = []
 
 for cond in db_condlist:
-    calc_mean = []
-    for j in new_data[0]:
+    calc_arr = []
+    calc_mean_final = []
+    for j in new_data[3]:
         parameters_input = []
         user_input = []
         finding_input = {}
@@ -89,20 +105,35 @@ for cond in db_condlist:
             name = params[param]
             value = j[param]
             parameters_input.append((name, value))
-            finding_input["parameters"] = parameters_input[::-1]
+            finding_input["parameters"] = parameters_input
         user_input.append(finding_input)
-        cursor_findings = [db.fill.find(get_f_params_val("Mammography", cond, ui)).count() for ui in user_input]
-        calc_mean += cursor_findings
+        cursor_findings = [db.test_entr.find(get_f_params_val("Mammography", cond, ui)).count() for ui in user_input]
+        calc_arr += cursor_findings
+    a = cond_freq(cond)
+    if sum(calc_arr) !=0:
+        norm = [float(i) / sum(calc_arr) for i in calc_arr]
+        calc_mean_final += norm
+        #print(calc_mean_final, "h")
+        """"
+        s = pd.Series(calc_mean_final)
+        a = s* cond_freq(cond)
+        print(a)
+        """
+        temp = [i * a for i in calc_mean_final]
+        print(temp)
+        final = sum(temp)
+        print(final)
+    else:
+        continue
+    calc_cond = mean(calc_mean_final)
+    print("Calc mean for", cond, "is: ", calc_cond, "test is: ", final)
 
-    norm = [float(i) / sum(calc_mean) for i in calc_mean]
-    calc_mean_final = mean(norm)
-    calc_cond += [calc_mean_final]
-    print("Calc mean for", cond, "is: ", calc_mean_final)
-
+ #*f/ci
 
 for cond in db_condlist:
-    lnodes_mean = []
-    for j in new_data[1]:
+    lnodes_arr = []
+    lnodes_mean_final = []
+    for j in new_data[0]:
         parameters_input = []
         user_input = []
         finding_input = {}
@@ -112,19 +143,26 @@ for cond in db_condlist:
             name = params[param]
             value = j[param]
             parameters_input.append((name, value))
-            finding_input["parameters"] = parameters_input[::-1]
+            finding_input["parameters"] = parameters_input
         user_input.append(finding_input)
-        cursor_findings = [db.fill.find(get_f_params_val("Mammography", cond, ui)).count() for ui in user_input]
-        lnodes_mean += cursor_findings
+        cursor_findings = [db.test_entr.find(get_f_params_val("Mammography", cond, ui)).count() for ui in user_input]
+        lnodes_arr += cursor_findings
+        print(lnodes_arr, cond, user_input)
 
-    norm = [float(i) / sum(lnodes_mean) for i in lnodes_mean]
-    lnodes_mean_final = mean(norm)
-    lnodes_cond += [lnodes_mean_final]
-    print("Lymph mean for", cond, "is: ", lnodes_mean_final)
+    if sum(lnodes_arr) !=0:
+        norm = [float(i) / sum(lnodes_arr) for i in lnodes_arr]
+        print(norm)
+        lnodes_mean_final += norm
+        print(lnodes_mean_final)
+    else:
+        pass
 
+    lnodes_cond = mean(lnodes_mean_final)
+    print("Lnodes mean for", cond, "is: ", lnodes_cond)
+"""
 for cond in db_condlist:
     mass_mean = []
-    for i in new_data[2]:
+    for i in new_data[1]:
         parameters_input = []
         test_r = i
         user_input =[]
@@ -135,9 +173,9 @@ for cond in db_condlist:
             name = params[param]
             value = i[param]
             parameters_input.append((name,value))
-            finding_input["parameters"] = parameters_input[::-1]
+            finding_input["parameters"] = parameters_input
         user_input.append(finding_input)
-        cursor_findings = [db.fill.find(get_f_params_val("Mammography", cond, ui)).count() for ui in user_input]
+        cursor_findings = [db.test_entr.find(get_f_params_val("Mammography", cond, ui)).count() for ui in user_input]
         mass_mean+=cursor_findings
 
     norm = [float(i) / sum(mass_mean) for i in mass_mean]
@@ -148,7 +186,7 @@ for cond in db_condlist:
 
 for cond in db_condlist:
     assym_mean = []
-    for j in new_data[3]:
+    for j in new_data[2]:
         parameters_input = []
         user_input = []
         finding_input = {}
@@ -158,24 +196,23 @@ for cond in db_condlist:
             name = params[param]
             value = j[param]
             parameters_input.append((name, value))
-            finding_input["parameters"] = parameters_input[::-1]
+            finding_input["parameters"] = parameters_input
         user_input.append(finding_input)
-        cursor_findings = [db.fill.find(get_f_params_val("Mammography", cond, ui)).count() for ui in user_input]
+        cursor_findings = [db.test_entr.find(get_f_params_val("Mammography", cond, ui)).count() for ui in user_input]
         assym_mean += cursor_findings
 
     norm = [float(i) / sum(assym_mean) for i in assym_mean]
     assym_mean_final = mean(norm)
     assym_cond += [assym_mean_final]
     print("Assym mean for", cond, "is: ", assym_mean_final)
-
+"""
 
 d = {'condlist': db_condlist,'mass': mass_cond, 'calc': calc_cond, 'lymph nodes': lnodes_cond, 'assym': assym_cond}
 df = pd.DataFrame(data = d)
 df.to_csv('testmammo3', header=True, index = True)
 
 import xlsxwriter
-writer_orig = pd.ExcelWriter('testmammo3.xlsx', engine='xlsxwriter')
+writer_orig = pd.ExcelWriter('testmammo4.xlsx', engine='xlsxwriter')
 df.to_excel(writer_orig, index=True, sheet_name='report')
 writer_orig.save()
 
-#normalize values
